6f79d03030e5c22761af92f140b75420730b3a38
Hossain Khademian <hco@post.com>
2019-06-05 11:59:37 +0430

    add useful links for app

--- src/main/java/ir/hossainco/utils/App.kt
+++ src/main/java/ir/hossainco/utils/App.kt
@@ -7,4 +7,7 @@ object App {
 	inline val context get() = app.applicationContext
 	inline val res get() = app.resources
 	inline val assets get() = app.assets
+
+	val label get() = app.getString(app.applicationInfo.labelRes)
+	val appLink get() = "http://play.google.com/store/apps/details?id=${app.packageName}"
 }
